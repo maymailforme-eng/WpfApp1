a31b876690b3326bfa3bf25798fe52dfb17adae1
Лев <maymailforme@gmail.com>
2026-04-25 19:31:45 +0300

    BindingInProgress

---
+++ Binding/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿//using System;
+//using System.Collections.Generic;
+using System.ComponentModel;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
+
+namespace Binding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        string boundText;
+        public string BoundText
+        { 
+            get => boundText;
+            set
+            { 
+                boundText = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+            txtInput.Focus();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(BoundText)));
+        }
+
+        private void btnDefault_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "Default";
+        }
+    }
+}
